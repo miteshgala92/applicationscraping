e25a2567aaf39443262faefe41c1ddbb1f523ddb
mitesh.gala <galamitesh1005@gmail.com>
2020-11-16 11:39:26 +0400

    created credentials folder

--- flyin/src/googlesheettest.py
+++ flyin/src/googlesheettest.py
@@ -20,8 +20,8 @@ def routes_data():
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+    if os.path.exists('/Users/mitesh.gala/PycharmProjects/applicationscraping/flyin/credentials/token.pickle'):
+        with open('/Users/mitesh.gala/PycharmProjects/applicationscraping/flyin/credentials/token.pickle', 'rb') as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
@@ -29,10 +29,10 @@ def routes_data():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                '/Users/mitesh.gala/PycharmProjects/applicationscraping/flyin/credentials/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
+        with open('/Users/mitesh.gala/PycharmProjects/applicationscraping/flyin/credentials/token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('sheets', 'v4', credentials=creds)
